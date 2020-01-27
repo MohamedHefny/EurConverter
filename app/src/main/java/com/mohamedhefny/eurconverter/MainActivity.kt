8e00ceb035b0398836688d935eb600b3de568347
Mohamed Hefny <hefny.7@gmail.com>
2020-01-28 01:30:27 +0200

    First commit.

---
+++ app/src/main/java/com/mohamedhefny/eurconverter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mohamedhefny.eurconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
