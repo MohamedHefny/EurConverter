7a0dfe1d31bd248957cd959f52a29254963e37df
Mohamed Hefny <hefny.7@gmail.com>
2020-01-28 02:24:17 +0200

    Add currencies rates api service.

--- app/src/main/java/com/mohamedhefny/eurconverter/data/remote/ApiServices.kt
+++ app/src/main/java/com/mohamedhefny/eurconverter/data/remote/ApiServices.kt
@@ -1,5 +1,15 @@
 package com.mohamedhefny.eurconverter.data.remote
 
+import com.mohamedhefny.eurconverter.BuildConfig
+import com.mohamedhefny.eurconverter.data.models.CurrencyRatesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiServices {
 
+    @GET("latest")
+    suspend fun getEurRates(
+        @Query("format") format: Int = 1,
+        @Query("access_key") access_key: String = BuildConfig.API_KEY
+    ): CurrencyRatesResponse
 }
